9121ee47ff47e7ec415fc4a68ed9b42f13fbd314
Joshua Blum <joshblum@mit.edu>
2012-12-07 17:52:30 -0500

    admin interface

---
+++ audit_log/admin.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from django.contrib import admin
+
+
+def reg_admin():
+    for model in models.get_models():
+        if _check_name(model):
+            admin.site.register(model, DefaultAuditAdmin)
+
+def _check_name(cls):
+    return "AuditLogEntry" in cls.__name__
+
+class DefaultAuditAdmin(admin.ModelAdmin):
+    """
+        Generic admin interface for displaying audit objects
+    """
+    pass
+
+reg_admin()
