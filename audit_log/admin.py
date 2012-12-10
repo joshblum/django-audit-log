f75bad0dfc42a873c16670e6bc86b6da241dac3f
Joshua Blum <joshblum@mit.edu>
2012-12-09 21:28:25 -0500

    updated admin

--- audit_log/admin.py
+++ audit_log/admin.py
@@ -19,14 +19,12 @@ class DefaultAuditAdmin(admin.ModelAdmin):
         Generic admin interface for displaying audit objects
     """
 
-    def action_id_friendly(self, obj):
-        return obj.action_id
-
     def action_date_friendly(self, obj):
-        return '<b>'+str(obj.action_date.strftime('%m/%d/%y %I:%M:%S')) + ' UTC</b>'
+        return '%s UTC' %str(obj.action_date.strftime('%m/%d/%y %I:%M:%S'))
 
     def action_user_friendly(self, obj):
-        search_by_user = '<a href="?q=%s">%s</a>' % (obj.action_user, obj.action_user)
+        user = obj.action_user
+        search_by_user = '<a href="?q=%s">%s</a>' % (user, user)
         return search_by_user
 
     def action_type_friendly(self, obj):
@@ -42,7 +40,8 @@ class DefaultAuditAdmin(admin.ModelAdmin):
         return types[obj.action_type]
 
     def action_ip_friendly(self, obj):
-        search_by_ip = '<a href="?q=%s">%s</a>' % (obj.action_ip, obj.action_ip)
+        ip = obj.action_ip
+        search_by_ip = '<a href="?q=%s">%s</a>' % (ip, ip)
         return search_by_ip
     
     def action_referrer_friendly(self, obj):
@@ -55,8 +54,6 @@ class DefaultAuditAdmin(admin.ModelAdmin):
         if r.groups():
             res = r.groups()[0]
         return res
-
-    action_id_friendly.short_description = 'ID'
 
     action_date_friendly.allow_tags = True
     action_date_friendly.short_description = 'Timestamp'
@@ -73,12 +70,12 @@ class DefaultAuditAdmin(admin.ModelAdmin):
 
     action_user_agent_friendly.short_description = 'User Agent'
         
-    list_display = ('action_id_friendly', 'action_user_friendly', 'action_type_friendly', 'action_ip_friendly', 'action_referrer_friendly', 'action_user_agent_friendly', 'action_date_friendly')
+    list_display = ('action_user_friendly', 'action_type_friendly', 'action_ip_friendly', 'action_referrer_friendly', 'action_user_agent_friendly', 'action_date_friendly')
 
-    list_display_links = ('action_id_friendly', 'action_date_friendly')
+    list_display_links = ('action_date_friendly',)
 
     search_fields = ('action_user__username', 'action_referrer', 'action_ip', 'action_date', 'action_user_agent')
     list_filter = ('action_type',)
-    ordering = ['action_id']
+    ordering = ['-action_date']
 
 reg_admin()
